2218bec7727099a6d2966203e48d707907fb2325
Marcus Weiner <marcus.weiner@gmail.com>
2020-04-12 21:25:57 +0200

    Refactor github fs to use git data

--- previews/githubfs/file.go
+++ previews/githubfs/file.go
@@ -2,20 +2,24 @@ package githubfs
 
 import (
 	"bytes"
+	"context"
 	"errors"
+	"fmt"
+	"net/http"
 	"os"
 	"path/filepath"
-
-	"github.com/google/go-github/v31/github"
 )
 
+// githubFile is referencing a tree or blob
 type githubFile struct {
-	isDir bool
-	path  string
-	size  int64
+	fs *githubFS
 
-	files []*github.RepositoryContent
-	body  *bytes.Reader
+	id      string
+	objType string
+	path    string
+	size    int64
+
+	body *bytes.Reader
 }
 
 func (f *githubFile) Close() error {
@@ -23,27 +27,51 @@ func (f *githubFile) Close() error {
 }
 
 // ErrIsDir is returned whenever the caller tries to use a read operation on a directory
-var ErrIsDir = errors.New("Cannot do read operations on directories")
+var ErrIsDir = errors.New("Cannot do read operations on non-blobs")
+
+func (f *githubFile) initBody() error {
+	if f.body != nil {
+		return nil
+	}
+
+	if f.objType != "blob" {
+		return ErrIsDir
+	}
+
+	blob, resp, err := f.fs.client.Git.GetBlobRaw(context.Background(), f.fs.repoOwner, f.fs.repoName, f.id)
+	if err != nil {
+		if resp.StatusCode == http.StatusNotFound {
+			return errNotFound
+		}
+		if resp.StatusCode == http.StatusForbidden {
+			fmt.Printf("rate limit?\n%+v\n", resp.Header)
+		}
+		return err
+	}
+
+	f.body = bytes.NewReader(blob)
+	return nil
+}
 
 func (f *githubFile) Read(p []byte) (n int, err error) {
-	if f.isDir {
-		return 0, ErrIsDir
+	if err := f.initBody(); err != nil {
+		return 0, err
 	}
 
 	return f.body.Read(p)
 }
 
 func (f *githubFile) ReadAt(p []byte, off int64) (n int, err error) {
-	if f.isDir {
-		return 0, ErrIsDir
+	if err := f.initBody(); err != nil {
+		return 0, err
 	}
 
 	return f.body.ReadAt(p, off)
 }
 
 func (f *githubFile) Seek(offset int64, whence int) (int64, error) {
-	if f.isDir {
-		return 0, ErrIsDir
+	if err := f.initBody(); err != nil {
+		return 0, err
 	}
 
 	return f.body.Seek(offset, whence)
@@ -54,36 +82,46 @@ func (f *githubFile) Name() string {
 }
 
 func (f *githubFile) Readdir(count int) ([]os.FileInfo, error) {
-	if !f.isDir {
+	if f.objType != "tree" {
 		return nil, errors.New("Cannot list files, is not a directory")
 	}
 
-	infos := make([]os.FileInfo, len(f.files))
-	for i, file := range f.files {
-		infos[i] = &githubFileInfo{
-			isDir: file.GetType() == "dir",
-			name:  file.GetName(),
-			size:  int64(file.GetSize()),
-		}
+	tree, err := f.fs.getTree(f.path, f.id)
+	if err != nil {
+		return nil, err
+	}
+
+	infos := make([]os.FileInfo, len(tree))
+	var i int
+	for _, file := range tree {
+		infos[i], _ = file.Stat() // current impl guarantees no error
+		i++
 	}
 	return infos, nil
 }
 
 func (f *githubFile) Readdirnames(n int) ([]string, error) {
-	if !f.isDir {
+	if f.objType != "tree" {
 		return nil, errors.New("Cannot list files, is not a directory")
 	}
 
-	names := make([]string, len(f.files))
-	for i, file := range f.files {
-		names[i] = file.GetName()
+	tree, err := f.fs.getTree(f.path, f.id)
+	if err != nil {
+		return nil, err
+	}
+
+	names := make([]string, len(tree))
+	var i int
+	for _, file := range tree {
+		names[i] = filepath.Base(file.path)
+		i++
 	}
 	return names, nil
 }
 
 func (f *githubFile) Stat() (os.FileInfo, error) {
 	return &githubFileInfo{
-		isDir: f.isDir,
+		isDir: f.objType == "tree",
 		name:  filepath.Base(f.path),
 		size:  f.size,
 	}, nil
--- previews/githubfs/fs.go
+++ previews/githubfs/fs.go
@@ -1,12 +1,13 @@
 package githubfs
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
+	"sync"
 	"syscall"
 	"time"
 
@@ -22,6 +23,9 @@ type githubFS struct {
 	repoOwner string
 	repoName  string
 	branch    string
+
+	rootTree  string
+	treeCache sync.Map // map[string]map[string]*githubFile
 }
 
 // New creates an FS to get files from github on-demand
@@ -46,50 +50,87 @@ func New(accessToken string, repo string, branch string) (afero.Fs, error) {
 	}, nil
 }
 
-func (fs *githubFS) Open(name string) (afero.File, error) {
-	fmt.Printf("Opening file: %s\n", name)
-	path := strings.TrimPrefix(name, "/")
-	if strings.HasPrefix(path, "layouts/layouts") {
-		fmt.Println("bla!")
+func (fs *githubFS) getTree(basePath string, id string) (map[string]*githubFile, error) {
+	// lookup in cache
+	cacheTree, ok := fs.treeCache.Load(id)
+	if ok {
+		tree, ok := cacheTree.(map[string]*githubFile)
+		if ok {
+			return tree, nil
+		}
+		// invalid obj in cache, remove
+		fmt.Printf("invalid cache object: %T\n", cacheTree)
+		fs.treeCache.Delete(id)
 	}
 
-	file, dir, resp, err := fs.client.Repositories.GetContents(
-		context.Background(),
-		fs.repoOwner,
-		fs.repoName,
-		path,
-		&github.RepositoryContentGetOptions{Ref: fs.branch},
-	)
+	// get from API
+	ghTree, resp, err := fs.client.Git.GetTree(context.Background(), fs.repoOwner, fs.repoName, id, false)
 	if err != nil {
 		if resp.StatusCode == http.StatusNotFound {
-			fmt.Printf("not found: %s\n", name)
-			return nil, syscall.ENOENT // afero tries this in the copy on write impl
+			return nil, errNotFound
+		}
+		if resp.StatusCode == http.StatusForbidden {
+			fmt.Printf("rate limit?\n%+v\n", resp.Header)
 		}
 		return nil, err
 	}
 
-	if file != nil {
-		content, err := file.GetContent()
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to read content")
+	tree := make(map[string]*githubFile)
+	for _, entry := range ghTree.Entries {
+		fullPath := filepath.Join(basePath, entry.GetPath())
+		tree[entry.GetPath()] = &githubFile{
+			fs:      fs,
+			id:      entry.GetSHA(),
+			objType: entry.GetType(),
+			path:    fullPath,
+			size:    int64(entry.GetSize()),
 		}
-		return &githubFile{
-			isDir: false,
-			path:  file.GetPath(),
-			size:  int64(file.GetSize()),
-			body:  bytes.NewReader([]byte(content)),
-		}, nil
+	}
+	fs.treeCache.Store(id, tree)
+
+	return tree, nil
+}
+
+const errNotFound = syscall.ENOENT // afero tries this in the copy on write impl
+
+func (fs *githubFS) Open(name string) (afero.File, error) {
+	path := strings.TrimPrefix(name, "/")
+	parts := strings.Split(path, "/")
+
+	if fs.rootTree == "" {
+		if err := fs.loadRepoRootTree(); err != nil {
+			return nil, errors.Wrap(err, "Failed to load root tree")
+		}
 	}
 
-	if dir != nil {
-		return &githubFile{
-			isDir: true,
-			path:  name,
-			files: dir,
-		}, nil
+	var treeCursor string = fs.rootTree
+	var file *githubFile
+	for i, part := range parts {
+		if treeCursor == "" {
+			// in a previous loop run, there was a file there was not a tree
+			return nil, errNotFound
+		}
+
+		tree, err := fs.getTree(strings.Join(parts[:i], "/"), treeCursor)
+		if err != nil {
+			return nil, err
+		}
+
+		var ok bool
+		file, ok = tree[part]
+		if !ok {
+			return nil, errNotFound
+		}
+
+		if file.objType == "tree" {
+			treeCursor = file.id
+		}
 	}
 
-	return nil, errors.New("neither file nor dir returned")
+	if file == nil {
+		return nil, errNotFound
+	}
+	return file, nil
 }
 
 func (fs *githubFS) OpenFile(name string, flag int, perm os.FileMode) (afero.File, error) {
@@ -109,7 +150,7 @@ func (fs *githubFS) Stat(name string) (os.FileInfo, error) {
 	return file.Stat()
 }
 
-func (githubFS) Name() string {
+func (*githubFS) Name() string {
 	return "GithubFS"
 }
 
